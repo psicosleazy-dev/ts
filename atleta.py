8d9c7af1809086f527c20275b37c6537a5b5974f
Your Name <you@example.com>
2023-11-09 15:14:45 -0300

    novo atleta_test e fazendo tarefa de atleta

---
+++ atleta.py
@@ -0,0 +1,16 @@
+class Atleta:
+    def __init__(self, nome, modalidade, idade, categoria):
+        self.nome = nome
+        self.modalidade = modalidade
+        self.idade = idade
+        self.categoria = categoria
+
+    def classificaAtleta(idade):
+        if idade == 15 or idade == 16 or idade == 17:
+            return 'menor'
+        elif idade == 18 or idade == 19:
+            return 'juvenil'
+        elif idade >= 20 and idade <= 34:
+            return 'adulto'
+        else:
+            return 'invalido'
